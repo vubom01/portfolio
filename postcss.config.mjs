6c815e9da24cee39dd5fd789fb626a3a4d9ab60f
vu.lh <vu.lh@teko.vn>
2024-10-11 14:48:14 +0700

    update postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
