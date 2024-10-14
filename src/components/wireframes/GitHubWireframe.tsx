64859ef466c9fc3edd4792aaca6bd36846760f3d
vubom01 <lehuyvu1852001@gmail.com>
2024-10-14 13:44:00 +0700

    update github wireframe

--- src/components/wireframes/GitHubWireframe.tsx
+++ src/components/wireframes/GitHubWireframe.tsx
@@ -149,6 +149,10 @@ function GitHubWireframe({
           </div>
         </div>
       </div>
+      <div className={clsx('mt-4 flex flex-col gap-2')}>
+        <SkeletonSm w={320} />
+        <SkeletonSm w={200} />
+      </div>
     </div>
   );
 }
