8e37fb94e1277dc29d56e375495c052983640388
vulh <lehuyvu1852001@gmail.com>
2024-12-24 14:15:06 +0700

    update studio page info

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -72,37 +72,37 @@ export default function Studio() {
             <div className={clsx('data-table-contents')}>
               <table>
                 <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Detail</th>
-                  </tr>
+                <tr>
+                  <th>Name</th>
+                  <th>Detail</th>
+                </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>OS</td>
-                    <td>Windows 10 Home 64-bit</td>
-                  </tr>
-                  <tr>
-                    <td>CPU</td>
-                    <td>Intel Core i5-10400F 10th Gen</td>
-                  </tr>
-                  <tr>
-                    <td>RAM</td>
-                    <td>16GB 3200MHz DDR4</td>
-                  </tr>
-                  <tr>
-                    <td>GPU</td>
-                    <td>RTX 2060 6GB GDDR6</td>
-                  </tr>
-                  <tr>
-                    <td>Storage</td>
-                    <td>NVMe M.2 512GB Gen3x4</td>
-                  </tr>
+                <tr>
+                  <td>OS</td>
+                  <td>Windows 10 Home 64-bit</td>
+                </tr>
+                <tr>
+                  <td>CPU</td>
+                  <td>Intel Core i5-10400F 10th Gen</td>
+                </tr>
+                <tr>
+                  <td>RAM</td>
+                  <td>16GB 3200MHz DDR4</td>
+                </tr>
+                <tr>
+                  <td>GPU</td>
+                  <td>RTX 2060 6GB GDDR6</td>
+                </tr>
+                <tr>
+                  <td>Storage</td>
+                  <td>NVMe M.2 512GB Gen3x4</td>
+                </tr>
                 </tbody>
               </table>
             </div>
             <h2 className={clsx('heading heading-h3')}>Support Device</h2>
-            <p>Apple MacBook Air M2 2022 16GB 256GB</p>
+            <p>Apple MacBook Air M3 2024 16GB 512GB</p>
             <hr className={clsx('divider')} />
             <h2
               id="display"
@@ -111,35 +111,31 @@ export default function Studio() {
             >
               Display
             </h2>
-            <p>
-              I often play games with my friends, so a 24-inch monitor is ideal
-              for my needs.
-            </p>
             <div className={clsx('data-table-contents')}>
               <table>
                 <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Detail</th>
-                  </tr>
+                <tr>
+                  <th>Name</th>
+                  <th>Detail</th>
+                </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Model</td>
-                    <td>Asus TUF GAMING VG249Q3A</td>
-                  </tr>
-                  <tr>
-                    <td>Resolution</td>
-                    <td>FHD 1920x1080</td>
-                  </tr>
-                  <tr>
-                    <td>Aspect ratio</td>
-                    <td>16:9</td>
-                  </tr>
-                  <tr>
-                    <td>Refresh rate</td>
-                    <td>180 Hz</td>
-                  </tr>
+                <tr>
+                  <td>Model</td>
+                  <td>Asus TUF GAMING VG249Q3A</td>
+                </tr>
+                <tr>
+                  <td>Resolution</td>
+                  <td>FHD 1920x1080</td>
+                </tr>
+                <tr>
+                  <td>Aspect ratio</td>
+                  <td>16:9</td>
+                </tr>
+                <tr>
+                  <td>Refresh rate</td>
+                  <td>180 Hz</td>
+                </tr>
                 </tbody>
               </table>
             </div>
@@ -151,69 +147,36 @@ export default function Studio() {
             >
               Keyboard
             </h2>
-            <h2 className={clsx('heading heading-h3')}>Primary</h2>
+            {/*<h2 className={clsx('heading heading-h3')}>Primary</h2>*/}
             <div className={clsx('data-table-contents')}>
               <table>
                 <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Detail</th>
-                  </tr>
+                <tr>
+                  <th>Name</th>
+                  <th>Detail</th>
+                </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Case</td>
-                    <td>Krush65 PC / SS Black PVD</td>
-                  </tr>
-                  <tr>
-                    <td>PCB</td>
-                    <td>Hotswap 8KHz 1 mode</td>
-                  </tr>
-                  <tr>
-                    <td>Plate</td>
-                    <td>Alu / Top mount</td>
-                  </tr>
-                  <tr>
-                    <td>Switch</td>
-                    <td>KTT Meow 5PIN Linear 43g</td>
-                  </tr>
-                  <tr>
-                    <td>Keycap</td>
-                    <td>GMK Mictlan</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <h2 className={clsx('heading heading-h3')}>Secondary</h2>
-            <div className={clsx('data-table-contents')}>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Detail</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Case</td>
-                    <td>NJ68 MAX - AP</td>
-                  </tr>
-                  <tr>
-                    <td>PCB</td>
-                    <td>Hotswap 3 modes</td>
-                  </tr>
-                  <tr>
-                    <td>Plate</td>
-                    <td>Brass / Sandwich mount</td>
-                  </tr>
-                  <tr>
-                    <td>Switch</td>
-                    <td>MZ Z1 5PIN Linear 45g</td>
-                  </tr>
-                  <tr>
-                    <td>Keycap</td>
-                    <td>EPBT Miami</td>
-                  </tr>
+                <tr>
+                  <td>Case</td>
+                  <td>Krush65 PC / SS Black PVD</td>
+                </tr>
+                <tr>
+                  <td>PCB</td>
+                  <td>Hotswap 8KHz 1 mode</td>
+                </tr>
+                <tr>
+                  <td>Plate</td>
+                  <td>Alu / Top mount</td>
+                </tr>
+                <tr>
+                  <td>Switch</td>
+                  <td>KTT Meow 5PIN Linear 43g</td>
+                </tr>
+                <tr>
+                  <td>Keycap</td>
+                  <td>GMK Mictlan</td>
+                </tr>
                 </tbody>
               </table>
             </div>
@@ -225,8 +188,62 @@ export default function Studio() {
             >
               Mouse
             </h2>
-            <p>VGN ATK F1 Ultimate - support 8KHz</p>
-            <p>VGN Dragonfly F1 MOBA - support 4KHz</p>
+            <div className={clsx('data-table-contents')}>
+              <table>
+                <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Detail</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Model</td>
+                  <td>VGN Dragonfly F1 MOBA / 2 modes</td>
+                </tr>
+                <tr>
+                  <td>Sensors</td>
+                  <td>Pixart PAW3395</td>
+                </tr>
+                <tr>
+                  <td>Size</td>
+                  <td>121,2mm x 63,5mm x 37,6mm</td>
+                </tr>
+                <tr>
+                  <td>Weight</td>
+                  <td>55g</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className={clsx('data-table-contents')}>
+              <table>
+                <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Detail</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Model</td>
+                  <td>VGN VXE R1 SE+ / 3 modes</td>
+                </tr>
+                <tr>
+                  <td>Sensors</td>
+                  <td>PAW3395 SE</td>
+                </tr>
+                <tr>
+                  <td>Size</td>
+                  <td>120,6mm x 64mm x 37,8mm</td>
+                </tr>
+                <tr>
+                  <td>Weight</td>
+                  <td>49g</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
             <hr className={clsx('divider')} />
             <h2
               id="audio-devices"
@@ -238,32 +255,32 @@ export default function Studio() {
             <div className={clsx('data-table-contents')}>
               <table>
                 <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Detail</th>
-                  </tr>
+                <tr>
+                  <th>Name</th>
+                  <th>Detail</th>
+                </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Speaker</td>
-                    <td>Divoom DiToo Pro</td>
-                  </tr>
-                  <tr>
-                    <td>Speaker</td>
-                    <td>Marshall Emberton 2</td>
-                  </tr>
-                  <tr>
-                    <td>Earbuds</td>
-                    <td>EarPods 3.5mm</td>
-                  </tr>
-                  <tr>
-                    <td>Earbuds</td>
-                    <td>Sony WF-1000XM5</td>
-                  </tr>
-                  <tr>
-                    <td>Headphones</td>
-                    <td>Sony WH-1000XM5</td>
-                  </tr>
+                <tr>
+                  <td>Speaker</td>
+                  <td>Divoom DiToo Pro</td>
+                </tr>
+                <tr>
+                  <td>Speaker</td>
+                  <td>Marshall Emberton 2</td>
+                </tr>
+                <tr>
+                  <td>Earbuds</td>
+                  <td>EarPods 3.5mm</td>
+                </tr>
+                <tr>
+                  <td>Earbuds</td>
+                  <td>Sony WF-1000XM5</td>
+                </tr>
+                <tr>
+                  <td>Headphones</td>
+                  <td>Sony WH-1000XM5</td>
+                </tr>
                 </tbody>
               </table>
             </div>
@@ -278,24 +295,24 @@ export default function Studio() {
             <div className={clsx('data-table-contents')}>
               <table>
                 <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Detail</th>
-                  </tr>
+                <tr>
+                  <th>Name</th>
+                  <th>Detail</th>
+                </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Music player</td>
-                    <td>Apple iPod Shuffle Gen 4</td>
-                  </tr>
-                  <tr>
-                    <td>Mouse pad</td>
-                    <td>X-raypad Aqua Control 2</td>
-                  </tr>
-                  <tr>
-                    <td>Keyboard pad</td>
-                    <td>NRKB Mictlan</td>
-                  </tr>
+                <tr>
+                  <td>Music player</td>
+                  <td>Apple iPod Shuffle Gen 4</td>
+                </tr>
+                <tr>
+                  <td>Mouse pad</td>
+                  <td>X-raypad Aqua Control 2</td>
+                </tr>
+                <tr>
+                  <td>Keyboard pad</td>
+                  <td>NRKB Mictlan</td>
+                </tr>
                 </tbody>
               </table>
             </div>
