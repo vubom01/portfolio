336fbaa601c95fe953996c01326f818fee5ae3a2
vubom01 <lehuyvu1852001@gmail.com>
2026-08-03 11:58:23 +0700

    update base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
+const basePath = '/portfolio';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath,
+  assetPrefix: basePath,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
 };
 
 export default nextConfig;
