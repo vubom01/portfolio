f2e9aa79c13a0e1b63c4527fc94ac1e76cee5446
vubom01 <lehuyvu1852001@gmail.com>
2026-08-03 10:54:46 +0700

    update workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+};
 
 export default withNextIntl(nextConfig);
